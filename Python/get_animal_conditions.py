db837d9e8a1e7249f7602125203539387852b5ae
Isaac Perron <ijperron@gmail.com>
2020-06-06 17:16:43 -0400

    update animal_conditions

--- Python/get_animal_conditions.py
+++ Python/get_animal_conditions.py
@@ -1,5 +1,6 @@
 def main():
     import pandas as pd
+    import os
     
     config_dict = {}
     for l in open('config.txt', 'r') .readlines():
@@ -9,8 +10,9 @@ def main():
         config_dict[l.split(' = ')[0]] = l.split(' = ')[1].split('\n')[0]
 
     animal_conditions = pd.DataFrame()
+
     for f in os.listdir(config_dict['file_directory']):
-        if (f.split('.')[1] == 'csv') & (f != id_condition_filename):
+        if (f.split('.')[1] == 'csv') & (f != config_dict['id_condition_filename']):
             print(f)
 
             dict_ac = {'raw_filename':f,
@@ -21,7 +23,7 @@ def main():
 
             animal_conditions = animal_conditions.append(pd.DataFrame(dict_ac,index=[0]),ignore_index=True, sort=False)
 
-    animal_conditions.to_csv(id_condition_filename,index=False)
+    animal_conditions.to_csv(config_dict['id_condition_filename'],index=False)
     
 if __name__ == "__main__":
     main()
