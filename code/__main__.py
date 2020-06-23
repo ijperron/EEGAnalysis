2cf82fdcfc9ccbd9966dd611327d70b0e426a30a
Isaac Perron <ijperron@gmail.com>
2020-06-23 19:12:55 -0400

    moved init and main

--- code/__main__.py
+++ code/__main__.py
@@ -1,4 +1,5 @@
 import os, sys
+sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), 'code'))
 
 import numpy as np
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="eeg-sleep-analysis", # Replace with your own username
+    name="eeg-sleep-analysis",
     version="1.0.0",
     author="Isaac J. Perron",
     author_email="ijperron@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ijperron/EEGAnalysis",
-    packages=setuptools.find_packages(),
+    packages=["code"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
