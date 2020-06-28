bdad9197522d5e979722839cb8c0eedf24798df0
Isaac Perron <ijperron@gmail.com>
2020-06-28 14:23:09 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eeg-sleep-analysis",
-    version="1.0.0",
+    version="1.0.1",
     author="Isaac J. Perron",
     author_email="ijperron@gmail.com",
     description="Package to analyze EEG-scored sleep",
